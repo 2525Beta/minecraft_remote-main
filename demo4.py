9082bb7e2d6403c85d19fce3be69db5d4e90a50d
佐藤祥成 <85811822+2525Beta@users.noreply.github.com>
2022-07-13 22:01:54 +0900

    upd
    upd

--- demo4.py
+++ demo4.py
@@ -1,18 +1,16 @@
+from curses import tparm
 from mcje.minecraft import Minecraft
 import param_MCJE as param
 import axis_flat
 import time
-
-Materials = param.GOLD_BLOCK
-X,Y,Z = -10,3,-10
-
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat("接続完了")
 
+def set_house(x=-10, y=63, z=-10, material=param.GOLD_BLOCK):
+    mc.setBlocks( 0, 3, 0, -5, 8, 5, param.STONE)
+    mc.setBlocks(-1, 4, 1, -4, 7, 4, param.AIR)
+    mc.setBlocks( 0, 4, 4,  0, 5, 4, param.AIR)
 
-mc.setBlocks( X, Y, Z, X-5, Y+5, Z+5, Materials)
-mc.setBlocks(X-1, Y+1, Z+1, X-4, Y+4, Z+4, param.AIR)
-mc.setBlocks( X, Y+1, Z+4,  X, Y+2, Z+4, param.AIR)
+    mc.setBlocks( 0, 5, 2,  0, 6, 1, param.GLASS)
 
-mc.setBlocks( X, Y+2, Z+2,  X, Y+3, Z+1, param.GLASS)
-mc.setBlocks(X-4, Y+1, Z+5, param.CRAFTING_BLOCK)
+set_house()
