d0756a096f46bd2ace0606c4b8b5e804933d2581
佐藤祥成 <85811822+2525Beta@users.noreply.github.com>
2022-08-21 09:29:18 +0900

    upd
    upd

--- demo5.py
+++ demo5.py
@@ -1,19 +1,18 @@
 from mcje.minecraft import Minecraft
-from minecraft_remote.param_MCJE import DIAMOND_BLOCK
 import param_MCJE1122 as param
-import axis_flat
-from time import sleep
 
 
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat("接続完了")
-def make_house(X=0, Y=3, Z=0, Materials = param.DIAMOND_BLOCK):
-    #スーパーフラット用なので普通の世界で作成するときはY座標をいじってください
-    mc.setBlocks( X, Y, Z, X-5, Y+5, Z+5, Materials)
-    mc.setBlocks(X-1, Y+1, Z+1, X-4, Y+4, Z+4, param.AIR)
-    mc.setBlocks( X, Y+1, Z+4,  X, Y+2, Z+4, param.AIR)
 
-    mc.setBlocks( X, Y+2, Z+2,  X, Y+3, Z+1, param.GLASS)
-    mc.setBlocks(X-4, Y+1, Z+5, param.CRAFTING_BLOCK)
+
+def make_house(x=0, y=3, z=0, material = param.DIAMOND_BLOCK):
+    #スーパーフラット用なので普通の世界で作成するときはY座標をいじってください
+    mc.setBlocks(x,     y,     z,     x - 5, y + 5, z + 5, material)
+    mc.setBlocks(x - 1, y + 1, z + 1, x - 4, y + 4, z + 4, param.AIR)
+    mc.setBlocks(x,     y + 1, z + 4, x,     y + 2, z + 4, param.AIR)
+
+    mc.setBlocks(x,     y + 2, z + 2,  x, y + 3, z + 1, param.GLASS)
+    mc.setBlocks(x - 4, y + 1, z + 5, param.CRAFTING_BLOCK)
     
-make_house(X=7)
+make_house(x=7)
